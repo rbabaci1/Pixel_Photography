58a8b4966b823cf6f3bc30f3b3b6a86e27883710
rabah babaci <4rabah@gmail.com>
2020-04-13 17:41:17 -0700

    define the reducer function

---
+++ app/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  searchTerm: '',
+  photos: [],
+  error: false,
+  isLoading: false,
+};
+
+const reducer = (state = initialState, { type, payload }) => {
+  return state;
+};
