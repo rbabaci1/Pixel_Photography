64fec66c880b5d04807a75ea7c3eca9232b7274f
rabah babaci <4rabah@gmail.com>
2020-04-13 18:01:52 -0700

    connect the App to the store and test it

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchAllPictures } from './actions/actions';
 
-function App() {
+function App({ test, fetchAllPictures }) {
   return (
     <div className='App'>
       <h1>Pixels Photography</h1>
+
+      <button onClick={fetchAllPictures} style={{ fontSize: '26px' }}>
+        Test store
+      </button>
+
+      {test && <h2>Store connected. {test}</h2>}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    test: state.test,
+  };
+};
+
+export default connect(mapStateToProps, { fetchAllPictures })(App);
