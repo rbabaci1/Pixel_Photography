e2233d0be147ed153eb14ffcde7c85331e1a8cff
rabah babaci <4rabah@gmail.com>
2020-04-15 11:16:46 -0700

    polish the search bar

--- app/src/components/SearchForm.js
+++ app/src/components/SearchForm.js
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { fetchAllImages } from '../actions';
 
-function SearchForm({ fetchAllImages }) {
+function SearchForm({ fetchAllImages, loading }) {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetchAllImages(
-      `https://pixabay.com/api/?key=14685436-4a2efb015ccaa4b983c6b66ae&per_page=200&page=1&q=${searchTerm}`
-    );
-    setSearchTerm('');
+    if (searchTerm) {
+      fetchAllImages(
+        `https://pixabay.com/api/?key=14685436-4a2efb015ccaa4b983c6b66ae&per_page=200&page=1&q=${searchTerm}`
+      );
+      setSearchTerm('');
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        {'🔍'}
+        <button type='submit' disabled={loading}>
+          <span role='img' aria-label='search'>
+            🔍
+          </span>
+        </button>
+
         <input
           type='text'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          disabled={loading}
         />
       </label>
+
+      {loading && <CircularProgress color='secondary' />}
     </form>
   );
 }
 
-export default connect(null, { fetchAllImages })(SearchForm);
+export default connect(
+  ({ loading }) => ({
+    loading,
+  }),
+  { fetchAllImages }
+)(SearchForm);
