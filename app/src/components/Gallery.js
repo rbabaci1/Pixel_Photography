18ed5094b9de710ab34cd3d2ee12fcd2e0845956
rabah babaci <4rabah@gmail.com>
2020-04-14 21:14:15 -0700

    define the search form

--- app/src/components/Gallery.js
+++ app/src/components/Gallery.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ImageCard from './ImageCard';
 import { NavLink } from 'react-router-dom';
+import ImageCard from './ImageCard';
+import SearchForm from './SearchForm';
 
 function Gallery({ images, error }) {
   return error ? (
     <p style={{ textAlign: 'center', color: 'red' }}>{error}</p>
   ) : (
-    <div className='gallery'>
-      {images.map((image) => (
-        <ImageCard key={image.id} imageUrl={image.largeImageURL} />
-      ))}
-    </div>
+    <>
+      <SearchForm />
+
+      <div className='gallery'>
+        {images.map((image) => (
+          <ImageCard key={image.id} imageUrl={image.largeImageURL} />
+        ))}
+      </div>
+    </>
   );
 }
 
