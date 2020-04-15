f36f027c14e23f4af78a00fb2ee5419d85ed9ffc
rabah babaci <4rabah@gmail.com>
2020-04-15 12:27:45 -0700

    redirect to download image on click

--- app/src/components/ImageCard.js
+++ app/src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ImageCard({ imageUrl }) {
   return (
     <div className='image-card'>
-      <a download='foo.jpg' target='_blank' href={imageUrl}>
+      <a target='_blank' href={imageUrl}>
         <img src={imageUrl} alt='gallery' />
       </a>
     </div>
