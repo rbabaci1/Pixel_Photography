93f9ada27418f717dd11c63e34dbf7c32c053a32
rabah babaci <4rabah@gmail.com>
2020-04-14 20:41:36 -0700

    adjust app and body height

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className='header'>
       <h1>Pixel Photography</h1>
-      <h3>Home</h3>
+      <Link to='/'>
+        <h3>Home</h3>
+      </Link>
     </div>
   );
 }
