65e8de5241a039c807bec06d15f284951a09c2d6
rabah babaci <4rabah@gmail.com>
2020-04-15 20:39:28 -0700

    use HashRouter instead of BrowserRouter

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
